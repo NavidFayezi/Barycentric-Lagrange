9d1c3b3eaf648b44751ce3a02d0dbae83aa081c5
NavidFayezi <navidfayezi.98@gmail.com>
2020-11-01 21:36:38 +0330

    add interpolate() function and add_point() function

--- barycentric_lagrange.py
+++ barycentric_lagrange.py
@@ -1,6 +1,5 @@
 
 
-
 class Point:
 
     def __init__(self, x, y):
@@ -10,10 +9,12 @@ class Point:
 
 class BarycentricLagrange:
 
+    # initialize the class with data points
     def __init__(self, data_points):
         self.data_points = data_points
         self.weights = []
 
+    # calculate the weight of data_points[point_index]
     def calculate_weight(self, point_index):
         result = 1
         for i in range(len(self.data_points)):
@@ -21,16 +22,37 @@ class BarycentricLagrange:
                 result *= self.data_points[point_index].x - self.data_points[i].x
         self.weights.insert(point_index, 1/result)
 
+    def interpolation(self, x):
+        numerator = 0
+        denominator = 0
+        for i in range(len(self.data_points)):
+            temp = self.weights[i] / (x-self.data_points[i].x)
+            denominator += temp
+            numerator += temp * self.data_points[i].y
+        return numerator/denominator
 
-
+    # add a single point to data points
+    def add_point(self, new_point):
+        for i in range(len(self.data_points)):    # check to see if the x attribute of the new point already exists.
+            if new_point.x == self.data_points[i].x:    # if so, only update the y attribute
+                self.data_points[i].y = new_point.y
+                return
+        # update weights
+        for i in range(len(self.data_points)):
+            self.weights[i] *= 1/(self.data_points[i].x - new_point.x)
+        self.data_points.append(new_point)
+        # calculate the weight of the new point
+        self.calculate_weight(len(self.data_points)-1)
 
 
 if __name__ == "__main__":
-    point1 = Point(4, 21.5)
-    point2 = Point(3.4, 117.10)
-    point3 = Point(5.2, 531.5)
-    dp = [point1, point2, point3]
+    point1 = Point(2, 4)
+    point2 = Point(3, 9)
+    point3 = Point(1, 1)
+    dp = [point1, point2]
     bl = BarycentricLagrange(dp)
     for i in range(len(dp)):
         bl.calculate_weight(i)
-    print(bl.weights)
+    print(bl.interpolation(5))
+    bl.add_point(point3)
+    print(bl.interpolation(5))
